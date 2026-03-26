8bd91ea9ee92841f425d7782445d1c97f42b6276
Mick Beer <Mickbr@protonmail.com>
2026-03-26 06:12:14 +0100

    fix: _worker.js alle statische bestanden, nick geen echte namen meer

--- _worker.js
+++ _worker.js
@@ -1,5 +1,8 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    
+    // Serve all static assets via ASSETS binding
     return env.ASSETS.fetch(request);
   }
 }
